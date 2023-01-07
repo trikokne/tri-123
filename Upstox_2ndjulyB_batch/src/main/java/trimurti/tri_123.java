412d98525185155e61107603d4cc9872e59d9541
trikokne58@gmail.com <trikokne58@gmail.com>
2023-01-07 08:36:59 +0530

    develop branch is created

--- Upstox_2ndjulyB_batch/src/main/java/trimurti/tri_123.java
+++ Upstox_2ndjulyB_batch/src/main/java/trimurti/tri_123.java
@@ -1,5 +1,15 @@
 package trimurti;
 
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
 public class tri_123 {
 
+
+	    @Test
+	    public void tri()
+	    {
+	    	Reporter.log("new branch is created",true);
+	    }
+		
 }
